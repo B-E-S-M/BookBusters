725e698ee9c98ec5838429311f03c4e0da5b4df6
motiveg <bcasipit91@gmail.com>
2018-12-06 12:52:00 -0800

    added datetimes additional image links

--- BookBusters/Models/Book.swift
+++ BookBusters/Models/Book.swift
@@ -22,9 +22,14 @@ class Book {
     let location: String?
     
     let image_link: String? // convert to URL type later if needed
+    let image_link2: String? // convert to URL type later if needed
+    let image_link3: String? // convert to URL type later if needed
     
     let seller_phone: String?
     let seller_email: String?
+    
+    let created_at: String?
+    let updated_at: String?
     
     init(dictionary: [String: Any]) {
         id = dictionary["id"] as? Int ?? -1
@@ -39,9 +44,27 @@ class Book {
         location = dictionary["location"] as? String ?? "Location not specified"
         
         image_link = dictionary["image_link"] as? String ?? ""
+        image_link2 = dictionary["image_link2"] as? String ?? ""
+        image_link3 = dictionary["image_link3"] as? String ?? ""
         
         seller_phone = dictionary["seller_phone"] as? String ?? "Phone number not specified"
         seller_email = dictionary["seller_email"] as? String ?? "Email not specified"
+        
+        // Format date string
+        let createdAtOriginalString = dictionary["created_at"] as! String
+        let updatedAtOriginalString = dictionary["updated_at"] as! String
+        let formatter = DateFormatter()
+        // Configure the input format to parse the date string
+        formatter.dateFormat = "E MMM d HH:mm:ss Z y"
+        // Convert String to Date
+        let creationDate = formatter.date(from: createdAtOriginalString)!
+        let updateDate = formatter.date(from: updatedAtOriginalString)!
+        // Configure output format
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        // Convert Date to String and set
+        created_at = formatter.string(from: creationDate)
+        updated_at = formatter.string(from: updateDate)
     }
     
     class func books(dictionaries: [[String: Any]]) -> [Book] {
@@ -65,9 +88,14 @@ class Book {
         print("Location: \(location ?? "Location not specified")")
         
         print("Image Link: \(image_link ?? "")")
+        print("Image Link 2: \(image_link2 ?? "")")
+        print("Image Link 3: \(image_link3 ?? "")")
         
         print("Seller Phone: \(seller_phone ?? "Phone number not specified")")
         print("Seller Email: \(seller_email ?? "Email not specified")")
+        
+        print("Created at: \(created_at ?? "Creation time not available")")
+        print("Updated at: \(updated_at ?? "Update time not available")")
     }
     
 }
